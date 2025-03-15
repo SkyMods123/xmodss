b1454d2be35730e4b9785fb01bfe3593d54ac720
SkyMods123 <skymodss@gmail.com>
2025-03-15 17:21:27 +0100

    Izmjene

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,10 @@
 import { FaustPage, getNextServerSideProps } from '@faustwp/core'
 import { gql } from '@/__generated__'
 import {
-    NcgeneralSettingsFieldsFragmentFragment,
-    OrderEnum,
-    PostObjectsConnectionOrderbyEnum,
-    PostsFilterPageQueryGetPostsQuery,
+	NcgeneralSettingsFieldsFragmentFragment,
+	OrderEnum,
+	PostObjectsConnectionOrderbyEnum,
+	PostsFilterPageQueryGetPostsQuery,
 } from '@/__generated__/graphql'
 import { FILTERS_OPTIONS } from '@/contains/contants'
 import React from 'react'
@@ -25,271 +25,278 @@ const T = getTrans()
 const GET_POSTS_FIRST_COMMON = 24
 
 interface ConTextQuery {
-    search: string | null
-    tagIn: number[]
-    authorIn: number[]
-    categoryIn: number[]
-    field: string
-    order: string
-    first: number | null
-    last: number | null
-    after: string | null
-    before: string | null
+	search: string | null
+	tagIn: number[]
+	authorIn: number[]
+	categoryIn: number[]
+	field: string
+	order: string
+	first: number | null
+	last: number | null
+	after: string | null
+	before: string | null
 }
 
 const Page: FaustPage<PostsFilterPageQueryGetPostsQuery> = (props) => {
-    const { posts } = props.data || {}
-    const router = useRouter()
+	const { posts } = props.data || {}
+	const router = useRouter()
 
-    //
-    const {} = useGetPostsNcmazMetaByIds({
-        posts: (posts?.nodes || []) as TPostCard[],
-    })
-    //
+	//
+	const {} = useGetPostsNcmazMetaByIds({
+		posts: (posts?.nodes || []) as TPostCard[],
+	})
+	//
 
-    const initPosts = (posts?.nodes as PostDataFragmentType[]) || []
-    const ctxQuery: ConTextQuery = props.__PAGE_VARIABLES__?.ctxQuery || {}
+	const initPosts = (posts?.nodes as PostDataFragmentType[]) || []
+	const ctxQuery: ConTextQuery = props.__PAGE_VARIABLES__?.ctxQuery || {}
 
-    const onCategoriesUpdated = (ids: number[]) => {
-        router.push({
-            query: removeEmptyKey({
-                ...ctxQuery,
-                categoryIn: ids,
-                first: null,
-                last: null,
-                after: null,
-                before: null,
-            }),
-        })
-    }
+	const onCategoriesUpdated = (ids: number[]) => {
+		router.push({
+			query: removeEmptyKey({
+				...ctxQuery,
+				categoryIn: ids,
+				first: null,
+				last: null,
+				after: null,
+				before: null,
+			}),
+		})
+	}
 
-    const onTagsUpdated = (ids: number[]) => {
-        router.push({
-            query: removeEmptyKey({
-                ...ctxQuery,
-                tagIn: ids,
-                first: null,
-                last: null,
-                after: null,
-                before: null,
-            }),
-        })
-    }
+	const onTagsUpdated = (ids: number[]) => {
+		router.push({
+			query: removeEmptyKey({
+				...ctxQuery,
+				tagIn: ids,
+				first: null,
+				last: null,
+				after: null,
+				before: null,
+			}),
+		})
+	}
 
-    const onAuthorsUpdated = (ids: number[]) => {
-        router.push({
-            query: removeEmptyKey({
-                ...ctxQuery,
-                authorIn: ids,
-                first: null,
-                last: null,
-                after: null,
-                before: null,
-            }),
-        })
-    }
+	const onAuthorsUpdated = (ids: number[]) => {
+		router.push({
+			query: removeEmptyKey({
+				...ctxQuery,
+				authorIn: ids,
+				first: null,
+				last: null,
+				after: null,
+				before: null,
+			}),
+		})
+	}
 
-    const onKeywordUpdated = (keyword: string) => {
-        router.push({
-            query: removeEmptyKey({
-                ...ctxQuery,
-                search: keyword,
-                first: null,
-                last: null,
-                after: null,
-                before: null,
-            }),
-        })
-    }
+	const onKeywordUpdated = (keyword: string) => {
+		router.push({
+			query: removeEmptyKey({
+				...ctxQuery,
+				search: keyword,
+				first: null,
+				last: null,
+				after: null,
+				before: null,
+			}),
+		})
+	}
 
-    const onClickNext = () => {
-        router.push({
-            query: removeEmptyKey({
-                ...ctxQuery,
-                after: posts?.pageInfo.endCursor,
-                first: ctxQuery.first,
-                before: null,
-                last: null,
-            }),
-        })
-    }
+	const onClickNext = () => {
+		router.push({
+			query: removeEmptyKey({
+				...ctxQuery,
+				after: posts?.pageInfo.endCursor,
+				first: ctxQuery.first,
+				before: null,
+				last: null,
+			}),
+		})
+	}
 
-    const onClickPrev = () => {
-        router.push({
-            query: removeEmptyKey({
-                ...ctxQuery,
-                first: null,
-                after: null,
-                before: posts?.pageInfo.startCursor,
-                last: ctxQuery.first,
-            }),
-        })
-    }
+	const onClickPrev = () => {
+		router.push({
+			query: removeEmptyKey({
+				...ctxQuery,
+				first: null,
+				after: null,
+				before: posts?.pageInfo.startCursor,
+				last: ctxQuery.first,
+			}),
+		})
+	}
 
-    const removeEmptyKey = (obj: Record<string, any>) => {
-        Object.keys(obj).forEach((key) => !obj[key] && delete obj[key])
-        return obj
-    }
+	const removeEmptyKey = (obj: Record<string, any>) => {
+		Object.keys(obj).forEach((key) => !obj[key] && delete obj[key])
+		return obj
+	}
 
-    function checkRouterQueryFilter(
-        routerQueryFilter: `${PostObjectsConnectionOrderbyEnum}/${OrderEnum}`,
-    ) {
-        if (!routerQueryFilter) {
-            return false
-        }
+	function checkRouterQueryFilter(
+		routerQueryFilter: `${PostObjectsConnectionOrderbyEnum}/${OrderEnum}`,
+	) {
+		// tra ve false neu khong co filter/ lan dau tien vao trang  / khi chua click vao filter nao
+		if (!routerQueryFilter) {
+			return false
+		}
 
-        const [field, order] = routerQueryFilter?.split('/')
-        return {
-            field: field as PostObjectsConnectionOrderbyEnum,
-            order: order as OrderEnum,
-        }
-    }
+		const [field, order] = routerQueryFilter?.split('/')
+		return {
+			field: field as PostObjectsConnectionOrderbyEnum,
+			order: order as OrderEnum,
+		}
+	}
 
-    return (
-        <>
-            <PageLayout
-                headerMenuItems={props.data?.primaryMenuItems?.nodes || []}
-                footerMenuItems={props.data?.footerMenuItems?.nodes || []}
-                pageFeaturedImageUrl={null}
-                pageTitle={T.Posts}
-                generalSettings={
-                    props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
-                }
-            >
-                <div className={`nc-PageCollection`}>
-                    <div className="container space-y-16 py-10 sm:space-y-20 lg:space-y-28 lg:pb-28 lg:pt-20">
-                        <div className="space-y-10">
-                            <header>
-                                <div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-500">
-                                    <FireIcon className="h-6 w-6" />
-                                    <span className="">{T['Explore']}</span>
-                                </div>
-                                <h1 className="block text-2xl font-semibold capitalize sm:text-3xl lg:text-4xl">
-                                    {T['Posts']}
-                                </h1>
-                            </header>
+	return (
+		<>
+			<PageLayout
+				headerMenuItems={props.data?.primaryMenuItems?.nodes || []}
+				footerMenuItems={props.data?.footerMenuItems?.nodes || []}
+				pageFeaturedImageUrl={null}
+				pageTitle={T.Posts}
+				generalSettings={
+					props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
+				}
+			>
+				<div className={`nc-PageCollection`}>
+					<div className="container space-y-16 py-10 sm:space-y-20 lg:space-y-28 lg:pb-28 lg:pt-20">
+						<div className="space-y-10">
+							<header>
+								<div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-500">
+									<FireIcon className="h-6 w-6" />
+									<span className="">{T['Explore']}</span>
+								</div>
+								<h1 className="block text-2xl font-semibold capitalize sm:text-3xl lg:text-4xl">
+									{T['Posts']}
+								</h1>
+							</header>
 
-                            <hr className="border-slate-200 dark:border-slate-700" />
+							<hr className="border-slate-200 dark:border-slate-700" />
 
-                            <main>
-                                <div className="flex flex-col lg:flex-row lg:justify-between">
-                                    <TabFilters
-                                        initCatIds={ctxQuery.categoryIn}
-                                        initTagIds={ctxQuery.tagIn}
-                                        initAuthorIds={ctxQuery.authorIn || []}
-                                        initKeyword={ctxQuery.search || ''}
-                                        onCategoriesUpdated={onCategoriesUpdated}
-                                        onTagsUpdated={onTagsUpdated}
-                                        onAuthorsUpdated={onAuthorsUpdated}
-                                        onKeywordUpdated={onKeywordUpdated}
-                                    />
-                                    <div className="my-4 block w-full border-b border-neutral-300 lg:hidden dark:border-neutral-500" />
-                                    <div className="flex justify-end">
-                                        <ArchiveFilterListBox
-                                            defaultValue={
-                                                FILTERS_OPTIONS.filter(
-                                                    (item) =>
-                                                        item.value ==
-                                                        `${ctxQuery.field.toUpperCase()}/${ctxQuery.order.toUpperCase()}`,
-                                                )[0]
-                                            }
-                                            onChange={(item) => {
-                                                const fiterValue = checkRouterQueryFilter(item.value)
-                                                if (!fiterValue) {
-                                                    return
-                                                }
-                                                router.push({
-                                                    query: removeEmptyKey({
-                                                        ...ctxQuery,
-                                                        first: null,
-                                                        last: null,
-                                                        after: null,
-                                                        before: null,
-                                                        field: fiterValue.field,
-                                                        order: fiterValue.order,
-                                                    }),
-                                                })
-                                            }}
-                                            lists={FILTERS_OPTIONS}
-                                        />
-                                    </div>
-                                </div>
+							<main>
+								{/* TABS FILTER */}
+								<div className="flex flex-col lg:flex-row lg:justify-between">
+									<TabFilters
+										initCatIds={ctxQuery.categoryIn}
+										initTagIds={ctxQuery.tagIn}
+										initAuthorIds={ctxQuery.authorIn || []}
+										initKeyword={ctxQuery.search || ''}
+										onCategoriesUpdated={onCategoriesUpdated}
+										onTagsUpdated={onTagsUpdated}
+										onAuthorsUpdated={onAuthorsUpdated}
+										onKeywordUpdated={onKeywordUpdated}
+									/>
+									<div className="my-4 block w-full border-b border-neutral-300 lg:hidden dark:border-neutral-500" />
+									<div className="flex justify-end">
+										<ArchiveFilterListBox
+											defaultValue={
+												FILTERS_OPTIONS.filter(
+													(item) =>
+														item.value ==
+														`${ctxQuery.field.toUpperCase()}/${ctxQuery.order.toUpperCase()}`,
+												)[0]
+											}
+											onChange={(item) => {
+												const fiterValue = checkRouterQueryFilter(item.value)
+												if (!fiterValue) {
+													return
+												}
+												router.push({
+													query: removeEmptyKey({
+														...ctxQuery,
+														first: null,
+														last: null,
+														after: null,
+														before: null,
+														field: fiterValue.field,
+														order: fiterValue.order,
+													}),
+												})
+											}}
+											lists={FILTERS_OPTIONS}
+										/>
+									</div>
+								</div>
 
-                                <GridPostsArchive
-                                    posts={initPosts}
-                                    showNextPagination={posts?.pageInfo.hasNextPage}
-                                    showPrevPagination={posts?.pageInfo.hasPreviousPage}
-                                    onClickNext={onClickNext}
-                                    onClickPrev={onClickPrev}
-                                />
-                            </main>
-                        </div>
-                    </div>
-                </div>
-            </PageLayout>
-        </>
-    )
+								{/* LOOP ITEMS */}
+								<GridPostsArchive
+									posts={initPosts}
+									showNextPagination={posts?.pageInfo.hasNextPage}
+									showPrevPagination={posts?.pageInfo.hasPreviousPage}
+									onClickNext={onClickNext}
+									onClickPrev={onClickPrev}
+								/>
+							</main>
+						</div>
+
+						{/* SUBCRIBES */}
+					</div>
+				</div>
+			</PageLayout>
+		</>
+	)
 }
 
 Page.variables = (context) => {
-    const { params, query = {} } = context as GetServerSidePropsContext
+	const { params, query = {} } = context as GetServerSidePropsContext
+	// query: { search: 'x', tagIn: ['1'], authorIn: ['2'], categoryIn: ['3'], page: '1', order: 'desc', field: 'date' }
 
-    const search = typeof query.search === 'string' ? query.search || null : null
+	const search = typeof query.search === 'string' ? query.search || null : null
 
-    const tagIn = (
-        Array.isArray(query.tagIn) ? query.tagIn : query.tagIn ? [query.tagIn] : []
-    ).map((id) => parseInt(id))
-    const authorIn = (
-        Array.isArray(query.authorIn)
-            ? query.authorIn
-            : query.authorIn
-                ? [query.authorIn]
-                : []
-    ).map((id) => parseInt(id))
-    const categoryIn = (
-        Array.isArray(query.categoryIn)
-            ? query.categoryIn
-            : query.categoryIn
-                ? [query.categoryIn]
-                : []
-    ).map((id) => parseInt(id))
+	const tagIn = (
+		Array.isArray(query.tagIn) ? query.tagIn : query.tagIn ? [query.tagIn] : []
+	).map((id) => parseInt(id))
+	const authorIn = (
+		Array.isArray(query.authorIn)
+			? query.authorIn
+			: query.authorIn
+				? [query.authorIn]
+				: []
+	).map((id) => parseInt(id))
+	const categoryIn = (
+		Array.isArray(query.categoryIn)
+			? query.categoryIn
+			: query.categoryIn
+				? [query.categoryIn]
+				: []
+	).map((id) => parseInt(id))
 
-    const field = typeof query.field === 'string' ? query.field : 'DATE'
-    const order = typeof query.order === 'string' ? query.order : 'DESC'
+	const field = typeof query.field === 'string' ? query.field : 'DATE'
+	const order = typeof query.order === 'string' ? query.order : 'DESC'
 
-    const after = typeof query.after === 'string' ? query.after || null : null
-    const before = typeof query.before === 'string' ? query.before || null : null
+	const after = typeof query.after === 'string' ? query.after || null : null
+	const before = typeof query.before === 'string' ? query.before || null : null
 
-    let first =
-        typeof query.first === 'string' ? parseInt(query.first) || null : null
-    let last =
-        typeof query.last === 'string' ? parseInt(query.last) || null : null
+	let first =
+		typeof query.first === 'string' ? parseInt(query.first) || null : null
+	let last =
+		typeof query.last === 'string' ? parseInt(query.last) || null : null
 
-    if (!after && !before) {
-        first = GET_POSTS_FIRST_COMMON
-        last = null
-    }
+	// lan dau request thi first = 20, last = null
+	if (!after && !before) {
+		first = GET_POSTS_FIRST_COMMON
+		last = null
+	}
 
-    const ctxQuery: ConTextQuery = {
-        ...query,
-        search,
-        tagIn,
-        authorIn,
-        categoryIn,
-        field,
-        order,
-        first: last ? null : first || GET_POSTS_FIRST_COMMON,
-        last,
-        after,
-        before,
-    }
+	const ctxQuery: ConTextQuery = {
+		...query,
+		search,
+		tagIn,
+		authorIn,
+		categoryIn,
+		field,
+		order,
+		first: last ? null : first || GET_POSTS_FIRST_COMMON,
+		last,
+		after,
+		before,
+	}
 
-    return {
-        ...ctxQuery,
-        ctxQuery,
-        headerLocation: PRIMARY_LOCATION,
-        footerLocation: FOOTER_LOCATION,
-    }
+	return {
+		...ctxQuery,
+		ctxQuery,
+		headerLocation: PRIMARY_LOCATION,
+		footerLocation: FOOTER_LOCATION,
+	}
 }
 
 Page.query = gql(`
@@ -363,9 +370,9 @@ Page.query = gql(`
 `)
 
 export function getServerSideProps(ctx: GetServerSidePropsContext) {
-    return getNextServerSideProps(ctx, {
-        Page,
-    })
+	return getNextServerSideProps(ctx, {
+		Page,
+	})
 }
 
 export default Page
