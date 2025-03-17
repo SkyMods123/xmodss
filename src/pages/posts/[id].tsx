7218ab706b0e2ee3a1914070bebe679f6322a55b
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-17 20:44:37 +0100

    Update [id].tsx

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -6,18 +6,22 @@ const SinglePage = ()=>{
             <Head>
                 <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="preload" as="image" href="https://rscripts.net/logo.svg" alt="RScripts Logo" />
+                <link rel="preload" as="image" href="https://rscripts.net/logo.svg" />
                 <link rel="stylesheet" href="./kola3_files/0d7c83ff46c8f2cd.css" />
                 <link rel="stylesheet" href="./kola3_files/e27b94790ece9e59.css" />
                 <link rel="stylesheet" href="./kola3_files/4932d2e513585f8a.css" />
             </Head>
-            <div className="rounded-lg border text-card-foreground shadow-sm bg-card/70 backdrop-blur-sm">
-                <div className="flex flex-col space-y-1.5 p-6 pb-4">
-                    <div className="flex flex-col gap-1">
-                        <div className="flex items-center space-x-4">
-                            {/* Sadržaj stranice */}
-                        </div>
+            <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
+                <div className="flex flex-col space-y-1.5 p-6">
+                    <div className="text-2xl font-semibold leading-none tracking-tight">
+                        <h2>Description</h2>
                     </div>
+                </div>
+                <div className="p-6 pt-0 space-y-4">
+                    <div className="description prose prose-invert max-w-none">
+                        <h2>ESP</h2>
+                    </div>
+                    <div className="mt-4 flex flex-col gap-4 sm:flex-row"></div>
                 </div>
             </div>
         </>
