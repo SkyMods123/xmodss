66fbfe62d14fe2ae86ddb9d21d310e92d9a20f0c
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-18 04:02:49 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -156,7 +156,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                     className="lucide lucide-copy mr-2 h-4 w-4"
                                   >
                                     <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
-                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                    <path class="lucide lucide-download h-4 w-4"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                                   </svg>
                                   Download
                                 </span>
