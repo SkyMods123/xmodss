438a654e50b130cd920bbcd425da51a7c5b2282c
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-18 03:42:42 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -175,7 +175,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
 					  </div>
 				  </div>
 				  <div className="p-6 pt-0 space-y-4">
-                            		  <div className="description prose prose-invert max-w-none">
+                            		  <div className="flex flex-col gap-2">
                                 		  <p><strong>Script Update! Dead Rails 1.0.1 Added Aimbot, Melee Kill Aura, Auto aim &amp; shoot, Gun Modifications, Auto Heal, No Bandage Delay Use</strong></p>
 					   </div>
 				  </div>		  
