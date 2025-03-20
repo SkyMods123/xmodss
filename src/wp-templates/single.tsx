bb5030fe5e06b85fca8d9d57db34d691d0da861d
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-20 18:31:04 +0100

    Update single.tsx

--- src/wp-templates/single.tsx
+++ src/wp-templates/single.tsx
@@ -191,16 +191,7 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
 					<div>
 						<div className={`relative`}>
 							{renderHeaderType()}
-
-							<div className="container my-10 flex flex-col lg:flex-row">
-								<div className="w-full lg:w-3/5 xl:w-2/3 xl:pe-20">
-									<SingleContent post={_post} />
-								</div>
-								<div className="mt-12 w-full lg:mt-0 lg:w-2/5 lg:ps-10 xl:w-1/3 xl:ps-0">
-									<Sidebar categories={_top10Categories} />
-								</div>
-							</div>
-
+							
 							{/* RELATED POSTS */}
 							<DynamicSingleRelatedPosts
 								posts={_relatedPosts}
