adf6d2e0a3533810192bbd0bec612d6c53af1aaa
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-20 01:36:41 +0100

    Update [id].tsx

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,38 +1,128 @@
-import React, { FC } from "react";
-import Heading from "@/components/Heading/Heading";
-import SectionSliderPosts from "@/components/Sections/SectionSliderPosts";
-import { PostDataFragmentType } from "@/data/types";
+import { TPostCard } from '@/components/Card2/Card2'
+import { TCategoryCardFull } from '@/components/CardCategory1/CardCategory1'
+import { MagazineLayoutType } from '@/wp-blocks/NcmazFaustBlockMagazineClient'
+import { TermBlockLayoutType } from '@/wp-blocks/NcmazFaustBlockTerms'
+import { useEffect, useRef, useState } from 'react'
 
-export interface SingleRelatedPostsProps3 {
-  postDatabaseId: number;
-  posts: PostDataFragmentType[] | null;
+const backendUrl = process.env.NEXT_PUBLIC_WORDPRESS_URL || ''
+
+const Page = () => {
+	const [data, setData] = useState<{
+		data?: TPostCard[] | TCategoryCardFull[] | null
+		blockVariation?: string | null
+		error: any
+		clientId: string
+	}>({ data: [], error: null, clientId: '' })
+	const [height, setHeight] = useState<number>()
+	const ref = useRef<HTMLDivElement>(null)
+	const [messageType, setMessageType] = useState<
+		'ncmazfc-preview-posts-block' | 'ncmazfc-preview-terms-block'
+	>()
+
+	useEffect(() => {
+		if (typeof window === 'undefined') {
+			return
+		}
+
+		const body = document.querySelector('body')
+		if (body) {
+			body.style.backgroundColor = 'transparent'
+			body.style.overflow = 'hidden'
+		}
+	}, [])
+
+	useEffect(() => {
+		if (typeof window === 'undefined') {
+			return
+		}
+
+		window.addEventListener('message', function (event) {
+			if (
+				!event.origin.includes(backendUrl) &&
+				!backendUrl.includes(event.origin)
+			) {
+				return
+			}
+			if (
+				!event.data ||
+				(event.data.type !== 'ncmazfc-preview-posts-block' &&
+					event.data.type !== 'ncmazfc-preview-terms-block')
+			) {
+				return
+			}
+			setMessageType(event.data.type)
+			setData(event.data)
+		})
+	}, [])
+
+	useEffect(() => {
+		// pass the message to the parent
+		setTimeout(() => {
+			window.parent.postMessage(
+				{
+					type: 'ncmaz-preview-iframe-height',
+					height,
+					clientId: data?.clientId,
+				},
+				backendUrl,
+			)
+		}, 200)
+	}, [height, data?.clientId])
+
+	useEffect(() => {
+		// Chọn div mà bạn muốn theo dõi
+		const targetNode = ref.current
+
+		if (!targetNode) {
+			return
+		}
+
+		const observer = new MutationObserver((mutationsList, observer) => {
+			for (let mutation of mutationsList) {
+				if (mutation.type === 'childList') {
+					setTimeout(() => {
+						const h = targetNode?.getBoundingClientRect().height
+						setHeight(h)
+					}, 100)
+				}
+			}
+		})
+		// Đặt các cài đặt cho MutationObserver
+		const config = { childList: true }
+		// Bắt đầu theo dõi
+		observer.observe(targetNode, config)
+		return () => {
+			observer.disconnect()
+		}
+	}, [ref])
+
+	console.log('data', data)
+
+	return (
+		<div ref={ref} className="no-prose relative">
+			<>
+				{!!data?.data?.length &&
+					messageType === 'ncmazfc-preview-posts-block' && (
+						<MagazineLayoutType
+							posts={data.data as TPostCard[]}
+							error={data.error}
+							blockVariation={data.blockVariation}
+						/>
+					)}
+
+				{!!data?.data?.length &&
+					messageType === 'ncmazfc-preview-terms-block' && (
+						<TermBlockLayoutType
+							blockVariation={data.blockVariation}
+							dataInitErrors={data.error}
+							dataInitTerms={data.data as TCategoryCardFull[]}
+						/>
+					)}
+			</>
+
+			<div className="absolute inset-0 z-[1] bg-transparent" />
+		</div>
+	)
 }
 
-const SingleRelatedPosts3: FC<SingleRelatedPostsProps3> = ({
-  postDatabaseId,
-  posts,
-}) => {
-  if (!posts?.length) {
-    return <div className="py-5" />;
-  }
-
-  return (
-    <div className="bg-neutral-100/80 dark:bg-neutral-800 py-16 lg:py-20 mt-16 lg:mt-20">
-      {/* RELATED  */}
-      <div className="container">
-        <div>
-          <Heading
-            className="mb-10 text-neutral-900 dark:text-neutral-50"
-            desc=""
-          >
-            Related posts
-          </Heading>
-
-          <SectionSliderPosts postCardName="card7" posts={posts || []} />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default SingleRelatedPosts3;
+export default Page
