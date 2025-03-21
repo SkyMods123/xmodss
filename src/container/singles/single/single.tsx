bb5ff71fdc14061a71d431ef8cbcbceffba7f172
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 06:18:23 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -13,7 +13,7 @@ import SingleRelatedPosts from '@/container/singles/SingleRelatedPosts';
 
 const GET_RELATED_POSTS = gql`
   query GetRelatedPosts($databaseId: Int!) {
-    posts(where: { isRelatedOfPostId: $databaseId }, first: 3) {
+    posts(where: { isRelatedOfPostId }, first: 3) {
       nodes {
         databaseId
         title
