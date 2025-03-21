f25eecbecede638db0d850c9b4a0dabc913246e1
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 03:47:00 +0100

    Update SingleRelatedPosts.tsx

--- src/container/singles/SingleRelatedPosts.tsx
+++ src/container/singles/SingleRelatedPosts.tsx
@@ -11,14 +11,25 @@ export interface SingleRelatedPostsProps {
 const SingleRelatedPosts: FC<SingleRelatedPostsProps> = ({
   postDatabaseId,
   posts,
-}) => {
-  if (!posts?.length) {
-    return <div className="py-5" />;
-  }
+}) => {}
 
   return (
+    <div className="bg-neutral-100/80 dark:bg-neutral-800 py-16 lg:py-20 mt-16 lg:mt-20">
+      {/* RELATED  */}
+      <div className="container">
+        <div>
+          <Heading
+            className="mb-10 text-neutral-900 dark:text-neutral-50"
+            desc=""
+          >
+            Related posts
+          </Heading>
+
           <SectionSliderPosts postCardName="card7" posts={posts || []} />
-        );
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleRelatedPosts;
