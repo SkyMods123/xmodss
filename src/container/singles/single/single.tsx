d243872105814c09f3490cd5683cf05ea883f425
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 16:45:36 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -10,35 +10,8 @@ import useGetPostsNcmazMetaByIds from "@/hooks/useGetPostsNcmazMetaByIds";
 import { gql, useQuery } from '@apollo/client';
 import { TPostCard } from '@/components/Card2/Card2';
 import SingleRelatedPosts from '@/container/singles/SingleRelatedPosts';
+import { GET_RELATED_POSTS } from '@/fragments/queries';
 
-const GET_RELATED_POSTS = gql`
-  query GetRelatedPosts($databaseId: Int!) {
-    posts(where: { isRelatedOfPostId: $databaseId }, first: 4) {
-      nodes {
-        databaseId
-        title
-        uri
-        date
-        excerpt
-        featuredImage {
-          node {
-            sourceUrl
-            altText
-          }
-        }
-        author {
-          node {
-            name
-            uri
-            avatar {
-              url
-            }
-          }
-        }
-      }
-    }
-  }
-`;
 
 export interface SingleType1Props {
     post: FragmentTypePostFullFields;
@@ -59,9 +32,11 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
     } = getPostDataFromPostFragment(post || {});
 
     // Fetch related posts
-    const { data: relatedPostsData, loading, error } = useQuery(GET_RELATED_POSTS, {
-        variables: { databaseId: Number(databaseId) },
-        skip: !databaseId
+    const { data: relatedPostsData, loading: loadingRelatedPosts } = useQuery(GET_RELATED_POSTS, {
+      variables: {
+        databaseId: databaseId 
+    },
+      skip: !databaseId
     });
 
     const relatedPosts = relatedPostsData?.posts?.nodes || [];
