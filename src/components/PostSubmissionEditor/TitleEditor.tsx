dd982da9786c34b6ef6a8bf0594942d3de84ae8a
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 22:09:49 +0100

    Update TitleEditor.tsx

--- src/components/PostSubmissionEditor/TitleEditor.tsx
+++ src/components/PostSubmissionEditor/TitleEditor.tsx
@@ -6,44 +6,42 @@ import getTrans from '@/utils/getTrans'
 import Textarea from '@/components/Textarea/Textarea'
 
 interface Props {
-	onUpdate: (editor: Editor) => void
-	defaultTitle?: string
+  onUpdate: (editor: Editor) => void
+  defaultTitle?: string
 }
 
 const TitleEditor: FC<Props> = ({ onUpdate, defaultTitle = '' }) => {
-	const T = getTrans()
-	const editor = useEditor({
-		extensions: [
-			StarterKit,
-			Placeholder.configure({
-				placeholder: T.pageSubmission['New post title here…'],
-			}),
-		],
-		editorProps: {
-			attributes: {
-				class:
-					'focus:outline-none max-w-screen-md mx-auto text-neutral-900 font-semibold text-2xl sm:text-3xl lg:text-4xl xl:leading-[115%] xl:text-[2.75rem] dark:text-neutral-100',
-			},
-		},
-		immediatelyRender: false,
-		content: defaultTitle,
-		onUpdate: ({ editor }) => {
-			// @ts-ignore
-			onUpdate(editor)
-		},
-	})
+  const T = getTrans()
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: T.pageSubmission['New post title here…'],
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          'focus:outline-none max-w-screen-md mx-auto text-neutral-900 font-semibold text-2xl sm:text-3xl lg:text-4xl xl:leading-[115%] xl:text-[2.75rem] dark:text-neutral-100',
+      },
+    },
+    immediatelyRender: false,
+    content: defaultTitle,
+    onUpdate: ({ editor }) => {
+      // @ts-ignore
+      onUpdate(editor)
+    },
+  })
 
-	return (
-		<>
-			<Textarea
-				editor={editor}
-				className="mt-1"
-				placeholder="cheat title"
-				name="excerpt"
-			/>
-		</>
-	)
-
+  return (
+    <>
+      <Textarea
+        className="mt-1"
+        placeholder="cheat title"
+        name="excerpt"
+      />
+    </>
+  )
 }
 
 export default TitleEditor
