37d723cbadb697f4e1e64cbe6e7997282f472582
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 18:50:41 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -222,7 +222,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                         </div>
                                         <div className="p-6 pt-0 space-y-4">
                                             <div className="flex flex-col gap-2">
-                                                <h4 className="text-neutral-900 truncate text-xl font-bold sm:text-2xl dark:text-neutral-100">{excerpt}</h4>
+                                                <div className="text-neutral-900 text-neutral-900 description prose prose-invert max-w-nonedark:text-neutral-100 dark:text-neutral-100">{excerpt}</div>
                                             </div>
                                         </div>
                                     </div>
