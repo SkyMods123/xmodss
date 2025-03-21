8db13055538f9bed1b74f9820dbcddf0d3127d2f
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 20:12:34 +0100

    Update TitleEditor.tsx

--- src/components/PostSubmissionEditor/TitleEditor.tsx
+++ src/components/PostSubmissionEditor/TitleEditor.tsx
@@ -1,10 +1,8 @@
-import { FC, useRef } from 'react'
+import { FC } from 'react'
 import { useEditor, EditorContent, Editor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Placeholder from '@tiptap/extension-placeholder'
 import getTrans from '@/utils/getTrans'
-import Label from 'your-label-component-path' // Adjust the import path as necessary
-import Input from 'your-input-component-path' // Adjust the import path as necessary
 
 interface Props {
 	onUpdate: (editor: Editor) => void
@@ -22,7 +20,8 @@ const TitleEditor: FC<Props> = ({ onUpdate, defaultTitle = '' }) => {
 		],
 		editorProps: {
 			attributes: {
-				class: 'focus:outline-none max-w-screen-md mx-auto text-neutral-900 font-semibold text-2xl sm:text-3xl lg:text-4xl xl:leading-[115%] xl:text-[2.75rem] dark:text-neutral-100',
+				class:
+					'focus:outline-none max-w-screen-md mx-auto text-neutral-900 font-semibold text-2xl sm:text-3xl lg:text-4xl xl:leading-[115%] xl:text-[2.75rem] dark:text-neutral-100',
 			},
 		},
 		immediatelyRender: false,
@@ -33,17 +32,7 @@ const TitleEditor: FC<Props> = ({ onUpdate, defaultTitle = '' }) => {
 		},
 	})
 
-	return (
-		<>
-			<div className="EditProfileForm__firstName">
-				<Label>{T['First Name']}</Label>
-				<Input
-					<EditorContent className="focus:outline-none" editor={editor} />
-				/>
-			</div>
-
-		</>
-	)
+	return <EditorContent className="focus:outline-none" editor={editor} />
 }
 
 export default TitleEditor
