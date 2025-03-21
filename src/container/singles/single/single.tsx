24a1e367dffe94b65a47e6083a5649aeb201f9ed
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-21 17:04:00 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -194,7 +194,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                     <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
                                         <div className="flex flex-col space-y-1.5 p-6">
                                             <div className="text-2xl font-semibold leading-none tracking-tight">
-                                                <h2>Description</h2>
+                                                <h2>Description7</h2>
                                             </div>
                                         </div>
                                         <div className="p-6 pt-0 space-y-4">
