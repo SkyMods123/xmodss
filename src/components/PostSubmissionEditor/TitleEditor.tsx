227d46a5e2dfd7c50333bd817b1eb8c3da856cad
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-22 14:56:11 +0100

    Update TitleEditor.tsx

--- src/components/PostSubmissionEditor/TitleEditor.tsx
+++ src/components/PostSubmissionEditor/TitleEditor.tsx
@@ -3,46 +3,36 @@ import { useEditor, EditorContent, Editor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Placeholder from '@tiptap/extension-placeholder'
 import getTrans from '@/utils/getTrans'
-import Textarea from '@/components/Textarea/Textarea'
 
 interface Props {
-  onUpdate: (editor: Editor) => void
-  defaultTitle?: string
+	onUpdate: (editor: Editor) => void
+	defaultTitle?: string
 }
 
 const TitleEditor: FC<Props> = ({ onUpdate, defaultTitle = '' }) => {
-  const T = getTrans()
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Placeholder.configure({
-        placeholder: T.pageSubmission['New post title here…'],
-      }),
-    ],
-    editorProps: {
-      attributes: {
-        class:
-          'focus:outline-none max-w-screen-md mx-auto text-neutral-900 font-semibold text-2xl sm:text-3xl lg:text-4xl xl:leading-[115%] xl:text-[2.75rem] dark:text-neutral-100',
-      },
-    },
-    immediatelyRender: false,
-    content: defaultTitle,
-    onUpdate: ({ editor }) => {
-      onUpdate(editor)
-    },
-  })
+	const T = getTrans()
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			Placeholder.configure({
+				placeholder: T.pageSubmission['New post title here…'],
+			}),
+		],
+		editorProps: {
+			attributes: {
+				class:
+					'focus:outline-none max-w-screen-md mx-auto text-neutral-900 font-semibold text-2xl sm:text-3xl lg:text-4xl xl:leading-[115%] xl:text-[2.75rem] dark:text-neutral-100',
+			},
+		},
+		immediatelyRender: false,
+		content: defaultTitle,
+		onUpdate: ({ editor }) => {
+			// @ts-ignore
+			onUpdate(editor)
+		},
+	})
 
-  return (
-    <>
-      <EditorContent editor={editor} className="mt-1" />
-      <Textarea
-        editor={editor}
-        className="mt-1"
-        placeholder="cheat title"
-        name="excerpt"
-      />
-    </>
-  )
+	return <EditorContent className="focus:outline-none" editor={editor} />
 }
 
 export default TitleEditor
