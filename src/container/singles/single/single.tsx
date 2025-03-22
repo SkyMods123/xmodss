da02fc184917142dd44da1e23d3ad4e8868ef5f0
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-03-22 17:08:35 +0100

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -81,7 +81,6 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
             <div className="bg-background __className_3a0388 min-h-screen">
                 <div className="coverimg absolute -top-[380px] hidden h-[50rem] w-full opacity-30 blur-[2px] md:block">
                     <img
-                        src={featuredImage?.sourceUrl || ''}
                         fetchPriority="high"
                         loading="eager"
                         width="135"
@@ -230,7 +229,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                             </div>
                             <aside className="script-similar-scripts lg:col-span-1">
                                 <div className="rounded-lg border text-card-foreground shadow-sm sticky top-15 bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 h-[900px] ">
-                                    <div className="flex flex-col space-y-1.5 p-6 pb-3">
+                                    <div className="flex flex-col space-y-1.5 p-6 pb-3" fetchPriority="high">
                                       <SingleRelatedPosts
                                         posts={relatedPosts}
                                         postDatabaseId={databaseId}
