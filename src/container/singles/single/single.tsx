9e8bcaba7d99eb15138ea57a2c8f7f6987a7c3ef
SkyMods123 <59374835+SkyMods123@users.noreply.github.com>
2025-04-04 17:46:53 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -200,9 +200,9 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                             <aside className="script-similar-scripts lg:col-span-1">
                                 <div className="rounded-lg border text-card-foreground shadow-sm sticky top-15 bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:w-[400px] h-[900px] ">
                                     <PostCardLikeAction
-							            likeCount={ncPostMetaData?.likesCount || 0}
-							            postDatabseId={databaseId}
-						            />
+					    likeCount={ncPostMetaData?.likesCount || 0}
+					    postDatabseId={databaseId}
+				    />
                                     <div className="flex flex-col space-y-1.5 p-6 pb-3">
                                       <SingleRelatedPosts
                                         posts={relatedPosts}
